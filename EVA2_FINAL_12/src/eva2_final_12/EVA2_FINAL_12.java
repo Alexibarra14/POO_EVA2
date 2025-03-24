f01bb22ae67b880bdbe010736b62aa1637b1f36d
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2025-03-23 19:28:25 -0600

    Add files via upload

---
+++ EVA2_FINAL_12/src/eva2_final_12/EVA2_FINAL_12.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_final_12;
+
+/**
+ *
+ * @author aleja
+ */
+public class EVA2_FINAL_12 {
+      final static int valor=100;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //FINAL: PALABRA RESERVADA
+        //Constantes:
+        System.out.println("valor: "+valor);
+        //valor=200; No se puede modificar
+     docente dosen= new docente();
+     
+    }
+    
+}
+class persona{
+    
+}
+class Empleado extends persona{
+    
+}
+final class docente extends Empleado{// hasta aqui llega la herencia
+    
+}
+//class docenteKinder extends docente{}/ NO SE PUEDE
+    
