f749dcbc8c2d4a25cf029a0b861077134504b664
Alexibarra14 <L24550366@chihuahua2.tecnm.mx>
2025-03-26 18:22:01 -0600

    Add files via upload

---
+++ EVA2_HERENCIA_INTERFACES_P15/src/eva2_herencia_interfaces_p15/EVA2_HERENCIA_INTERFACES_P15.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_herencia_interfaces_p15;
+
+/**
+ *
+ * @author aleja
+ */
+public class EVA2_HERENCIA_INTERFACES_P15 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //SI SE PUEDEN HEREDAR LAS INTERFACES AQUI EL EJEMPLO:
+    }
+    
+}
+interface PRUEBA{
+    
+}
+interface Prueba2 extends PRUEBA{
+    
+}
